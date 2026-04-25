d92f49d399aa395ca5e499bf59bcda6bdc21846c
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2026-04-25 17:30:07 +0900

    Initial commit

---
+++ src/eslint.config.js
@@ -0,0 +1,36 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+import pluginTailwindcss from "eslint-plugin-tailwindcss"
+
+export default [
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        plugins: {
+            tailwindcss: pluginTailwindcss // Use the object format here
+        },
+        rules: {
+            "react/jsx-uses-react": "off",
+            "react/react-in-jsx-scope": "off",
+            "react/prop-types": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "tailwindcss/classnames-order": "warn",
+            "semi": ["error", "never"]
+        }
+    },
+    ...storybook.configs["flat/recommended"]
+];
